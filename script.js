77ac6816b322293f24fbdccd27f98c9528f769e4
mmdreza <m13r81a@gmail.come>
2024-02-18 16:24:45 +0330

    update name function

--- script.js
+++ script.js
@@ -4,7 +4,53 @@ const wrapperElm = $.querySelector(".wrapper")
 
 const selectBtn = $.querySelector(".select-btn")
 
+const options = $.querySelector(".options")
+const inputElem = $.querySelector("input")
+
+
+let countries = ["Afghanistan", "Algeria", "Argentina", "Australia", "Bangladesh", "Belgium", "Bhutan",
+                 "Brazil", "Canada", "China", "Denmark", "Ethiopia", "Finland", "France", "Germany",
+                 "Hungary", "Iceland", "India", "Indonesia", "Iran", "Italy", "Japan", "Malaysia",
+                 "Maldives", "Mexico", "Morocco", "Nepal", "Netherlands", "Nigeria", "Norway", "Pakistan",
+                 "Peru", "Russia", "Romania", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland",
+                 "Thailand", "Turkey", "Uganda", "Ukraine", "United States", "United Kingdom", "Vietnam"];
+
+
+    
+     function addCountries(){
+        let li = null
+        countries.forEach((country)=>{
+            li = `<li onclick="updateName(this)">${country}</li>`
+            console.log(li);
+            options.insertAdjacentHTML("beforeend" , li)
+        })
+     }    
+
 
 selectBtn.addEventListener("click",()=>{
     wrapperElm.classList.toggle("active")
 })
+
+
+function updateName(el){
+    wrapperElm.classList.remove("active")
+    selectBtn.firstElementChild.innerHTML = el.textContent
+    inputElem.value = ""
+
+        for( let option of options.children){
+            console.log(option);
+            if(option.innerHTML === el.textContent){
+
+                option.classList.add("selected")
+                
+            }else{
+
+                option.classList.remove("selected")
+
+            }
+
+    }
+
+
+}
+window.addEventListener("load" , addCountries())
